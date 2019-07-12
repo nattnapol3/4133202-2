2047c84eaa28f00f434e5119a9cbeefcdcc90aff
nattnapol3 <600112417005@bru.ac.th>
2019-07-12 15:34:40 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,17 @@
+<?php
+
+echo $_POST['user'];
+echo "Hello World !!!";
+
+$name = "Nattapon";
+echo "Mr." . $name;
+
+echo sum(5,3);
+//function
+function sum($A  ,$B){
+    return $A + $B;
+}
+?>
+
+<!--short term, use one line. -->
+<?='My name is Nick'?>
